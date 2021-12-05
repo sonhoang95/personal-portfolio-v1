61ead765245fe02aeee0e7781443c4557130b9d6
Son Nguyen <sonhoang95@gmail.com>
2021-12-05 10:57:54 -0600

    fixed issues with lazy/suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { Suspense, lazy } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 import { AnimatePresence } from 'framer-motion';
-
-const Home = lazy(() => import('./pages/Home'));
-const About = lazy(() => import('./pages/About'));
-const Portfolio = lazy(() => import('./pages/Portfolio'));
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   const location = useLocation();
@@ -13,19 +11,17 @@ function App() {
     <>
       <ScrollToTop />
       <AnimatePresence exitBeforeEnter>
-        <Suspense fallback={<h1>Loading...</h1>}>
-          <Switch location={location} key={location.pathname}>
-            <Route exact path="/">
-              <Home title="Son Nguyen | Personal Website" />
-            </Route>
-            <Route path="/about">
-              <About title="About | Son Nguyen" />
-            </Route>
-            <Route path="/portfolio">
-              <Portfolio title="Portfolio | Son Nguyen" />
-            </Route>
-          </Switch>
-        </Suspense>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path="/">
+            <Home title="Son Nguyen | Personal Website" />
+          </Route>
+          <Route path="/about">
+            <About title="About | Son Nguyen" />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio title="Portfolio | Son Nguyen" />
+          </Route>
+        </Switch>
       </AnimatePresence>
     </>
   );
